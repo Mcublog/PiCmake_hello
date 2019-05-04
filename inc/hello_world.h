bd9dcca6188b14a9e407f68f00430249f259073f
Viacheslav <slava_ne@inbox.ru>
2019-05-04 19:06:27 +0300

    first commti

---
+++ inc/hello_world.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+
+#include <stdio.h>
+
+#define GRETEETING_STR "Hello, World from ARM!\n"
+
+#endif // HELLO_WORLD_H
---
+++ src/hello_wolrd.c
@@ -0,0 +1,7 @@
+#include "hello_world.h"
+
+int main(int argc, char** argv)
+{
+        printf(GRETEETING_STR);
+        return 0;
+}
